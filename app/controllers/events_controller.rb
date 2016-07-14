d6dff98db9ba51d9a4a054cd8ada6357b96f9a42
takuya <sui.shien@gmail.com>
2016-07-14 21:04:35 +0900

    イベント作成・更新・削除をユーザと紐付け

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -19,11 +19,11 @@ class EventsController < ApplicationController
   end
 
   def edit
-    @event = Event.find(params[:id])
+    @event = current_user.created_events.find(params[:id])
   end
 
   def update
-    @event = Event.find(params[:id])
+    @event = current_user.created_events.find(params[:id])
     if @event.update(event_params)
       redirect_to @event, notice: '更新しました'
     else
