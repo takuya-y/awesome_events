2fe853551ff16e6cf699da629d4a5ba45e916942
takuya <sui.shien@gmail.com>
2016-07-14 22:42:12 +0900

    イベント参加機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  resources :events
+
   root 'welcome#index'
 
   get '/auth/:provider/callback' => 'sessions#create'
   get '/logout' => 'sessions#destroy', as: :logout
+
+  resources :events do
+    resources :tickets
+  end
 
 end
